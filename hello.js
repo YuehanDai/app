6e9ab470cd2062e0ec5ca021da1503493be894b0
YuehanDai <78472561+YuehanDai@users.noreply.github.com>
2021-04-25 14:37:36 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res)
 	  
 	  if (req.url == "/")
 	  {
-		  file = 'formpage.html';
+		  file = 'form.html';
 		  fs.readFile(file, function(err, txt) {
     	  res.writeHead(200, {'Content-Type': 'text/html'});
 		  res.write("This is the home page<br>");
