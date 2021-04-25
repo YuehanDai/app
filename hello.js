7056b35d1c53e81774ebacbb224d5b17bcfa7395
YuehanDai <78472561+YuehanDai@users.noreply.github.com>
2021-04-25 17:57:26 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Yuri:IfMHnDqi7h8eFnzr@cluster0.p5voe.mongodb.net/textbooks?retryWrites=true&w=majority";
 var http = require('http');
 var fs = require('fs');
 var qs = require('querystring');
 	
 http.createServer(function (req, res) 
-  {
-	  
+  {	  
 	  if (req.url == "/")
 	  {
-		  file = 'form.html';
+		  file = 'StockTickerApp.html';
 		  fs.readFile(file, function(err, txt) {
     	  res.writeHead(200, {'Content-Type': 'text/html'});
 		  res.write("This is the home page<br>");
@@ -23,21 +24,47 @@ http.createServer(function (req, res)
 		 req.on('data', data => {
            pdata += data.toString();
          });
-
 		// when complete POST data is received
 		req.on('end', () => {
-			pdata = qs.parse(pdata);
-			res.write ("The select is: "+ pdata['select']);
-			res.end();
+			pdata = qs.parse(pdata);    
+            // on means when we have the data
+            findType = pdata['select'];
+            MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, db) {
+                if(err) { 
+                    console.log("Connection err: " + err); return; 
+                }
+                if (findType == "stock"){
+                    theQuery = {Ticker: {$regex: ".*" + pdata['StockName'] + ".*"}};          
+                } else {
+                    theQuery = {Company: {$regex: ".*" + pdata['CompanyName'] + ".*"}};             
+                }
+
+                var dbo = db.db("Assignment14");
+                var coll = dbo.collection('companies');
+
+                coll.find(theQuery).toArray(function(err, items) {
+                if (err) {
+                    res.write("Error: " + err);
+                } 
+                else 
+                {
+                    res.write("Items: </br>");
+                    if (items.length == 0){
+                        res.write("Not found");
+                    } else {
+                        for (i=0; i<items.length; i++)
+                        res.write(i + ": " + items[i].Company + " by: " + items[i].Ticker + "</br>");		
+                    }  		
+                }   
+                db.close();      
+                });//end find	   
+            });  //end connect            
 		});
-		// on means when we have the data
 	  }
 	  else 
 	  {
 		  res.writeHead(200, {'Content-Type':'text/html'});
 		  res.write ("Unknown page request");
 		  res.end();
-	  }
-  
-
-});
+	  } 
+}).listen(8080);
